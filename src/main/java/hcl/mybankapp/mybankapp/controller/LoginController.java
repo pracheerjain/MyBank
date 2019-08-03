127f3484bb23f713e9a51011448cc57ad5dbaa73
sagar8gaikwad <sagar8gaikwad@gmail.com>
2019-08-03 12:51:21 +0530

    initial commit

--- src/main/java/hcl/mybankapp/mybankapp/controller/LoginController.java
+++ src/main/java/hcl/mybankapp/mybankapp/controller/LoginController.java
@@ -1,5 +1,6 @@
 package hcl.mybankapp.mybankapp.controller;
 
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import hcl.mybankapp.mybankapp.dto.CustomerDTO;
+import hcl.mybankapp.mybankapp.exception.ApplicationException;
 
 @RestController
 @RequestMapping("/login")
@@ -14,13 +16,22 @@ import hcl.mybankapp.mybankapp.dto.CustomerDTO;
 public class LoginController {
 
 	@PostMapping("/validate")
-	public String validateUser(@RequestBody CustomerDTO inCustomer) {
+	public String validateUser(@RequestBody CustomerDTO inCustomer) throws ApplicationException {
+		validate(inCustomer);
 		return null;
 		
 	}
 	
 	
-	private void validate(CustomerDTO inCustomer) {
+	private void validate(CustomerDTO inCustomer) throws ApplicationException{
+		
+		if(StringUtils.isEmpty(inCustomer.getCustomerId())) {
+			throw new ApplicationException("customer id should not be empty");
+		}
+		
+		if(StringUtils.isEmpty(inCustomer.getPassword())) {
+			throw new ApplicationException("Password should not be empty");
+		}
 		
 	}
 	
