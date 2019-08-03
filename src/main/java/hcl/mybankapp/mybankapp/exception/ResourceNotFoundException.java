bc68476e6364a3a8e758c77e8b30461048555275
sontrive <sontrive0310@gmail.com>
2019-08-03 13:18:58 +0530

    commit

--- src/main/java/hcl/mybankapp/mybankapp/exception/ResourceNotFoundException.java
+++ src/main/java/hcl/mybankapp/mybankapp/exception/ResourceNotFoundException.java
@@ -1,5 +1,24 @@
 package hcl.mybankapp.mybankapp.exception;
 
-public class ResourceNotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+	public class ResourceNotFoundException extends RuntimeException {
+	    
+		private static final long serialVersionUID = 1L;
+		
+	    public ResourceNotFoundException() {
+	        super();
+	    }
+	    public ResourceNotFoundException(String message, Throwable cause) {
+	        super(message, cause);
+	    }
+	    public ResourceNotFoundException(String message) {
+	        super(message);
+	    }
+	    public ResourceNotFoundException(Throwable cause) {
+	        super(cause);
+	    }
 
 }
