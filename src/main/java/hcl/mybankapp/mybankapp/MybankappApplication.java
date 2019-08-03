88cefab9a6ef21c6e102986aecbb7606ebbdebc3
shivanjali kanase <shivanjali.kanase@sitpune.edu.in>
2019-08-03 11:24:47 +0530

    initial commit.

---
+++ src/main/java/hcl/mybankapp/mybankapp/MybankappApplication.java
@@ -0,0 +1,13 @@
+package hcl.mybankapp.mybankapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybankappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybankappApplication.class, args);
+	}
+
+}
