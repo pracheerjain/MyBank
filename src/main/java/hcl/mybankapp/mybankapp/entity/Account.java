c9e785aa1be77820a7e96df8e8915a6b6b296618
pracheerjain <pracheer2002@gmail.com>
2019-08-03 14:16:40 +0530

    fund transfer validation coding

--- src/main/java/hcl/mybankapp/mybankapp/entity/Account.java
+++ src/main/java/hcl/mybankapp/mybankapp/entity/Account.java
@@ -8,12 +8,16 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
+
+import org.hibernate.annotations.GenericGenerator;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
@@ -37,8 +41,13 @@ public class Account implements Serializable {
 	@Column(name = "id")
 	private Long id;
 
-	@OneToOne()
-	@JoinColumn(name = "customer_id")
+	/*
+	 * @OneToOne()
+	 * 
+	 * @JoinColumn(name = "customer_id")
+	 */
+	@OneToOne(fetch = FetchType.LAZY)
+	@PrimaryKeyJoinColumn
 	private Long customerId;
 
 	@Column(name = "account_type")
