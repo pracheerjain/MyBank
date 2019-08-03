b19507eca650b6065e3b50191b82e44893a6c572
shivanjali kanase <shivanjali.kanase@sitpune.edu.in>
2019-08-03 13:18:43 +0530

    initial commit.

--- src/main/java/hcl/mybankapp/mybankapp/repository/AccountRepository.java
+++ src/main/java/hcl/mybankapp/mybankapp/repository/AccountRepository.java
@@ -3,7 +3,7 @@ package hcl.mybankapp.mybankapp.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import hcl.mybankapp.mybankapp.service.Account;
+import hcl.mybankapp.mybankapp.entity.Account;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account,Long> {
